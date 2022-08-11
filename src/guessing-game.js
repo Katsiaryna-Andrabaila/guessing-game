a300d352083f759a5457062796cf4fb8c6183a50
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-08-11 17:42:44 +0300

    feat: done guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.number = number
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.setRange(this.min, this.guess());
     }
 
     greater() {
-
+        this.setRange(this.guess(), this.max);
     }
 }
 
